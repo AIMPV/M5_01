360da9eb82248299e2c81f97e5d7aa507d99e3de
Pablo Vallejos <pvallejos@aimmanager.com>
2024-04-27 00:04:51 -0400

    .

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://AIMPV.github.io/M5_01"
+  base: "https://AIMPV.github.io/M5_01",
+  build:{
+    outDir: "build"
+  }
 })
