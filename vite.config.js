a5a9fa542b3b82cfeaa300bdae53914be7b5b13a
Pablo Vallejos <pvallejos@aimmanager.com>
2024-04-27 00:06:55 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import InjectCSS from '@itsy/vite-css-inject';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "https://AIMPV.github.io/M5_01",
-  build:{
-    outDir: "build"
-  }
+  plugins: [
+    react(),
+    InjectCSS(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@tests": path.resolve(__dirname, "./tests")
+    }
+  },
+  base: "https://AIMPV.github.io/M5_01"
 })
